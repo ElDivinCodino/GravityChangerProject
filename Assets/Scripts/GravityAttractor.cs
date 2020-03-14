2c77d2a27f987c2ea18dd97569c73256db85f17d
Francesco <francesco.azetta@unitn.it>
2020-03-15 00:02:00 +0100

    Added working gravity

---
+++ Assets/Scripts/GravityAttractor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GravityAttractor : MonoBehaviour {
+	
+	public float gravity = -9.81f;
+	public bool isCentrifugal = false;
+
+	Vector3 gravityUp;
+	
+	public void Attract(Rigidbody body) {
+		gravityUp = isCentrifugal ? (transform.position - body.position).normalized : (body.position - transform.position).normalized;
+		
+		// Apply downwards gravity to body
+		body.AddForce(gravityUp * gravity);
+		// Align bodies up axis with the centre of planet
+		body.rotation = Quaternion.FromToRotation(body.transform.up, gravityUp) * body.rotation;
+	} 
+}
