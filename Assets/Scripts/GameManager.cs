3dd823b9e6c51b9329bb96f0890ebe75a0060e89
Francesco <francesco.azetta@unitn.it>
2020-03-22 11:39:52 +0100

    Adedd forgotten elements

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    GravityChanger[] objects;
+
+    void Start() 
+    {
+        objects = GameObject.FindObjectsOfType(typeof(GravityChanger)) as GravityChanger[];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire2"))
+        {
+            InvertGravity();
+        }
+    }
+
+    private void InvertGravity()
+    {
+        Physics.gravity *= -1;
+
+        RotateObjects();
+    }
+
+    private void RotateObjects()
+    {
+        foreach(GravityChanger g in objects)
+            g.RotateToGravity();
+    }
+}
---
+++ Assets/Scripts/GravityChanger.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class GravityChanger : MonoBehaviour
+{
+    public float rotationSpeedMultiplier = 1;
+
+    float rotationSpeed;
+	Ray ray;
+	RaycastHit hit;
+
+    Quaternion targetRot;
+
+    public void RotateToGravity()
+    {
+        rotationSpeed = GetRequiredSpeed();
+        targetRot = transform.rotation * Quaternion.Euler(180, 0, 0);
+        StartCoroutine(Rotate());
+    }
+
+    IEnumerator Rotate()
+    {
+        float speed = rotationSpeed * rotationSpeedMultiplier;
+        Quaternion previousRot = transform.rotation;
+
+        for(float t = 0f; t < 1f; t += speed * Time.deltaTime)
+        {
+            transform.rotation = Quaternion.Lerp(previousRot, targetRot, t);
+            yield return null;
+        }
+        
+        GetComponent<Rigidbody>().velocity = Vector3.zero;
+
+        ray = new Ray(transform.position, transform.up);
+
+        if (Physics.Raycast(ray, out hit, 100))
+        {
+			transform.rotation.SetLookRotation(transform.forward, transform.up);
+        }
+    }
+
+    float GetRequiredSpeed()
+    {
+        ray = new Ray(transform.position, transform.up);
+
+        if (Physics.Raycast(ray, out hit, 100))
+        {
+			return Vector3.Distance(transform.position, hit.point) * 0.05f;
+        }
+        else
+        {
+            return 0.5f;
+        }
+    }
+}
