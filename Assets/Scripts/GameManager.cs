428572189c0696143452ca4ed41ca93fab676ba9
Francesco <francesco.azetta@unitn.it>
2020-03-22 15:58:54 +0100

    Completed player movement on both gravities

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,7 +4,13 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [Tooltip("In seconds")]
+    public float gravityInversionCooldown;
+
     GravityChanger[] objects;
+
+    bool gravityInverted = false;
+    bool canInvertGravity = true;
 
     void Start() 
     {
@@ -14,15 +20,18 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire2"))
+        if (Input.GetButtonDown("Fire2") && canInvertGravity)
         {
+            canInvertGravity = false;
             InvertGravity();
+            StartCoroutine(EnableGravityInversion(gravityInversionCooldown));
         }
     }
 
     private void InvertGravity()
     {
         Physics.gravity *= -1;
+        gravityInverted = !gravityInverted;
 
         RotateObjects();
     }
@@ -31,5 +40,17 @@ public class GameManager : MonoBehaviour
     {
         foreach(GravityChanger g in objects)
             g.RotateToGravity();
+    }
+
+    public bool IsGravityInverted()
+    {
+        return gravityInverted;
+    }
+
+    IEnumerator EnableGravityInversion(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+
+        canInvertGravity = true;
     }
 }
--- Assets/Scripts/GravityChanger.cs
+++ Assets/Scripts/GravityChanger.cs
@@ -6,17 +6,19 @@ using UnityEngine;
 public class GravityChanger : MonoBehaviour
 {
     public float rotationSpeedMultiplier = 1;
+    public GameManager gameManager;
+    public Transform rotationReference;
 
     float rotationSpeed;
-	Ray ray;
-	RaycastHit hit;
+    Ray ray;
+    RaycastHit hit;
 
     Quaternion targetRot;
 
     public void RotateToGravity()
     {
         rotationSpeed = GetRequiredSpeed();
-        targetRot = transform.rotation * Quaternion.Euler(180, 0, 0);
+        targetRot = transform.rotation * Quaternion.Euler(transform.right * 180);
         StartCoroutine(Rotate());
     }
 
@@ -25,19 +27,20 @@ public class GravityChanger : MonoBehaviour
         float speed = rotationSpeed * rotationSpeedMultiplier;
         Quaternion previousRot = transform.rotation;
 
-        for(float t = 0f; t < 1f; t += speed * Time.deltaTime)
+        for (float t = 0f; t < 1f; t += speed * Time.deltaTime)
         {
             transform.rotation = Quaternion.Lerp(previousRot, targetRot, t);
             yield return null;
         }
-        
+
         GetComponent<Rigidbody>().velocity = Vector3.zero;
 
-        ray = new Ray(transform.position, transform.up);
+        ray = new Ray(transform.position, -transform.up);
 
         if (Physics.Raycast(ray, out hit, 100))
         {
-			transform.rotation.SetLookRotation(transform.forward, transform.up);
+            Vector3 currentRotation = transform.rotation.eulerAngles;
+            transform.rotation = Quaternion.Euler(hit.normal.x, currentRotation.y, currentRotation.z);
         }
     }
 
@@ -47,7 +50,7 @@ public class GravityChanger : MonoBehaviour
 
         if (Physics.Raycast(ray, out hit, 100))
         {
-			return Vector3.Distance(transform.position, hit.point) * 0.05f;
+            return Vector3.Distance(transform.position, hit.point) * 0.05f;
         }
         else
         {
