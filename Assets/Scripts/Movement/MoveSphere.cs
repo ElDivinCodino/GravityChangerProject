3ddd55c33783e414fa23f29c278c9d040ffc8392
Francesco <francesco.azetta@unitn.it>
2020-03-28 15:36:49 +0100

    Added sphere movement script and CameraOrbit script

---
+++ Assets/Scripts/Movement/MoveSphere.cs
@@ -0,0 +1,317 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class MoveSphere : MonoBehaviour
+{
+    // Note that setting both max speeds to the same value can produce inconsistent results due to precision limitations. 
+    // It's better to make the max snap speed a bit higher or lower than the max speed.
+
+    [Tooltip("In metres/seconds")]
+    [SerializeField, Range(0f, 100f)]
+    float maxSpeed = 10f;
+
+    [Tooltip("Max speed at which the player remains snapped to the ground")]
+    [SerializeField, Range(0f, 100f)]
+    float maxSnapSpeed = 100f;
+
+    [Tooltip("Distance (in metres) used to check whether player must be snapped or not")]
+    [SerializeField, Min(0f)]
+    float probeDistance = 1f;
+
+    [Tooltip("Layers that represent ground")]
+    [SerializeField]
+    LayerMask probeMask = -1;
+
+    [Tooltip("Layers that represent stairs")]
+    [SerializeField]
+    LayerMask stairsMask = -1;
+
+    [SerializeField, Range(0f, 100f)]
+    float maxAcceleration = 10f, maxAirAcceleration = 1f;
+
+    [Tooltip("In metres.")]
+    [SerializeField, Range(0f, 10f)]
+    float jumpHeight = 2f;
+
+    [SerializeField, Range(0, 5)]
+    int maxAirJumps = 0;
+
+    [Tooltip("Maximum angle of an object's slope to be considered as ground.")]
+    [SerializeField, Range(0, 90)]
+    float maxGroundAngle = 25f;
+
+    [Tooltip("Maximum angle of a stair's slope to be climbed.")]
+    [SerializeField, Range(0, 90)]
+    float maxStairsAngle = 50f;
+
+    [Tooltip("Player input space configuration. If not set, player input space configuration is World Space.")]
+    [SerializeField]
+    Transform playerInputSpace = default;
+
+    Rigidbody body;
+    Vector3 velocity, desiredVelocity;
+    Vector3 contactNormal, steepNormal; // A steep contact is one that is too steep to count as ground, but isn't a ceiling or overhang. 
+    bool desiredJump;
+    int groundContactCount, steepContactCount;
+    bool OnGround => groundContactCount > 0;
+    bool OnSteep => steepContactCount > 0;
+    int jumpPhase;
+    float minGroundDotProduct, minStairsDotProduct;
+    int stepsSinceLastGrounded, stepsSinceLastJump;
+
+
+    void OnValidate()
+    {
+        minGroundDotProduct = Mathf.Cos(maxGroundAngle * Mathf.Deg2Rad);
+        minStairsDotProduct = Mathf.Cos(maxStairsAngle * Mathf.Deg2Rad);
+    }
+
+    void Awake()
+    {
+        body = GetComponent<Rigidbody>();
+        OnValidate();
+    }
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        Vector2 playerInput;
+        playerInput.x = Input.GetAxis("Horizontal");
+        playerInput.y = Input.GetAxis("Vertical");
+        playerInput = Vector2.ClampMagnitude(playerInput, 1f);
+
+        if (playerInputSpace)	// If the movement is relative to a transform
+        {
+            Vector3 forward = playerInputSpace.forward;
+            forward.y = 0f;
+            forward.Normalize();
+            Vector3 right = playerInputSpace.right;
+            right.y = 0f;
+            right.Normalize();
+            desiredVelocity = (forward * playerInput.y + right * playerInput.x) * maxSpeed;
+        }
+        else	// Otherwise the movement is relative to World Space
+        {
+            desiredVelocity = new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
+        }
+
+        // |= because we could have pressed the button during a frame out of fixedUpdate, hence the jump could be not happened but the variable resets to false anyway the next frame
+        desiredJump |= Input.GetButtonDown("Jump");
+    }
+
+    void FixedUpdate()
+    {
+        UpdateState();
+        AdjustVelocity();
+
+        if (desiredJump)
+        {
+            desiredJump = false;
+            Jump();
+        }
+
+        body.velocity = velocity;
+        ClearState();
+    }
+
+    void ClearState()
+    {
+        groundContactCount = steepContactCount = 0;
+        contactNormal = steepNormal = Vector3.zero;
+    }
+
+    void UpdateState()
+    {
+        stepsSinceLastGrounded += 1;    // WARNING: could cause integer overflow if the player is not grounded for some in-game months :)
+        stepsSinceLastJump += 1;    // WARNING: could cause integer overflow if the player doesn't jump for some in-game months
+        velocity = body.velocity;
+
+        if (OnGround || SnapToGround() || CheckSteepContacts())
+        {
+            stepsSinceLastGrounded = 0;
+
+            // Reset the jump phase only when player is more than one step after a jump was initiated, to avoid false landing
+            if (stepsSinceLastJump > 1)
+            {
+                jumpPhase = 0;
+            }
+
+            if (groundContactCount > 1)
+            {
+                contactNormal.Normalize();
+            }
+        }
+        else
+        {
+            contactNormal = Vector3.up;
+        }
+    }
+
+    void AdjustVelocity()
+    {
+        Vector3 xAxis = ProjectOnContactPlane(Vector3.right).normalized;
+        Vector3 zAxis = ProjectOnContactPlane(Vector3.forward).normalized;
+
+        float currentX = Vector3.Dot(velocity, xAxis);
+        float currentZ = Vector3.Dot(velocity, zAxis);
+
+        float acceleration = OnGround ? maxAcceleration : maxAirAcceleration;
+        float maxSpeedChange = acceleration * Time.deltaTime;
+
+        float newX =
+            Mathf.MoveTowards(currentX, desiredVelocity.x, maxSpeedChange);
+        float newZ =
+            Mathf.MoveTowards(currentZ, desiredVelocity.z, maxSpeedChange);
+
+        velocity += xAxis * (newX - currentX) + zAxis * (newZ - currentZ);
+    }
+
+    void Jump()
+    {
+        Vector3 jumpDirection;
+
+        // Check whether player is on the ground. If so, use the contact normal for the jump direction
+        if (OnGround)
+        {
+            jumpDirection = contactNormal;
+        }
+        // If not, check whether player is on something steep. If so, use the steep normal instead
+        else if (OnSteep)
+        {
+            jumpDirection = steepNormal;
+            jumpPhase = 0; // To make possible wall jumping into a new sequence of air jumps
+        }
+        // If player is n-jumping, use the contact normal
+        else if (maxAirJumps > 0 && jumpPhase <= maxAirJumps)
+        {
+            if (jumpPhase == 0) // Skip the first jump phase when air jumping to prevent air jumping one extra time after falling off a surface without jumping
+            {
+                jumpPhase = 1;
+            }
+            jumpDirection = contactNormal;
+        }
+        else
+        {
+            return;
+        }
+
+        stepsSinceLastJump = 0;
+        jumpPhase += 1;
+        float jumpSpeed = Mathf.Sqrt(-2f * Physics.gravity.y * jumpHeight);
+
+        jumpDirection = (jumpDirection + Vector3.up).normalized; // To increase vertical speed when jumping off a vertical wall
+
+        float alignedSpeed = Vector3.Dot(velocity, jumpDirection);
+        if (alignedSpeed > 0f)
+        {
+            jumpSpeed = Mathf.Max(jumpSpeed - alignedSpeed, 0f);
+        }
+
+        velocity += jumpDirection * jumpSpeed;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        EvaluateCollision(collision);
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        EvaluateCollision(collision);
+    }
+
+    // Evaluate multiple collisions and sum their normals to obtain an average collision vector
+    void EvaluateCollision(Collision collision)
+    {
+        float minDot = GetMinDot(collision.gameObject.layer);
+
+        for (int i = 0; i < collision.contactCount; i++)
+        {
+            Vector3 normal = collision.GetContact(i).normal;
+            if (normal.y >= minDot)
+            {
+                groundContactCount += 1;
+                contactNormal += normal;
+            }
+            else if (normal.y > -0.01f) // The dot product of a perfectly vertical wall should be zero, but let's be a bit lenient.
+            {
+                steepContactCount += 1;
+                steepNormal += normal;
+            }
+        }
+    }
+
+    // Align our desired velocity with the ground by projecting the velocity on a plane
+    Vector3 ProjectOnContactPlane(Vector3 vector)
+    {
+        return vector - contactNormal * Vector3.Dot(vector, contactNormal);
+    }
+
+    // Keeps player stuck to the ground if needed 
+    bool SnapToGround()
+    {
+        // If player has just jumped or it is on air because of it 
+        if (stepsSinceLastGrounded > 1 || stepsSinceLastJump <= 2)
+        {
+            return false;
+        }
+
+        float speed = velocity.magnitude;
+        if (speed > maxSnapSpeed)
+        {
+            return false;
+        }
+
+        if (!Physics.Raycast(body.position, Vector3.down, out RaycastHit hit, probeDistance, probeMask))
+        {
+            return false;
+        }
+
+        if (hit.normal.y < GetMinDot(hit.collider.gameObject.layer))
+        {
+            return false;
+        }
+
+        // Player just lost contact with the ground but is still above ground, so must snap to it.
+        groundContactCount = 1;
+        contactNormal = hit.normal;
+
+        // Adjust player's velocity to align with the ground
+        float dot = Vector3.Dot(velocity, hit.normal);
+        // The velocity might already point somewhat down, in which case realigning it would slow convergence to the ground. Adjust only if dot product is negative
+        if (dot > 0f)
+            velocity = (velocity - hit.normal * dot).normalized * speed;
+
+        return true;
+    }
+
+    // Returns the appropriate minimum for a given layer (to differenciate between normal surfaces/ground and stairs)
+    float GetMinDot(int layer)
+    {
+        // Support a mask for any combination of layers.
+        return (stairsMask & (1 << layer)) == 0 ? minGroundDotProduct : minStairsDotProduct;
+    }
+
+    // Try to convert the steep contacts into virtual ground, and return whether the operation succeded or not.
+    // Needed to make player being able to move a bit and jump while in a crevasse or in similar places where it previously got stuck.
+    bool CheckSteepContacts()
+    {
+        if (steepContactCount > 1)
+        {
+            steepNormal.Normalize();
+            if (steepNormal.y >= minGroundDotProduct)
+            {
+                steepContactCount = 0;
+                groundContactCount = 1;
+                contactNormal = steepNormal;
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ Assets/Scripts/Movement/OrbitCamera.cs
@@ -0,0 +1,203 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class OrbitCamera : MonoBehaviour
+{
+    [SerializeField]
+    Transform focus = default;
+
+    [Tooltip("To relax the movement of the camera without sticking to the player each frame")]
+    [SerializeField, Min(0f)]
+    float focusRadius = 1f;
+
+    [Tooltip("Speed of recentering the focus on the target")]
+    [SerializeField, Range(0f, 1f)]
+    float focusCentering = 0.75f;
+
+    [Tooltip("Speed of aligning the focus on the target's direction")]
+    [SerializeField, Min(0f)]
+    float alignDelay = 5f;
+
+    [Tooltip("Until this angle the rotation speed scales linearly with the difference between current and desired angle. After, it rotates at full speed.")]
+    [SerializeField, Range(0f, 90f)]
+    float alignSmoothRange = 45f;
+
+    [Tooltip("Camera rotation speed in degrees per second")]
+    [SerializeField, Range(1f, 360f)]
+    float rotationSpeed = 90f;
+
+    [SerializeField, Range(-89f, 89f)]
+    float minVerticalAngle = -30f, maxVerticalAngle = 60f;
+
+    [SerializeField, Range(1f, 20f)]
+    float distance = 5f;
+
+    [Tooltip("Layers of geometries that the camera can intersect.")]
+    [SerializeField]
+	LayerMask obstructionMask = -1;
+
+    public bool invertYAxis = false, invertXAxis = false;
+
+    Vector3 focusPoint, previousFocusPoint;
+    Vector2 orbitAngles = new Vector2(45f, 0f); // X angle defines vertical orientation, y angle defines horizontal orientation
+    float lastManualRotationTime;
+    Camera regularCamera;
+
+    void Awake()
+    {
+        regularCamera = GetComponent<Camera>();
+        focusPoint = focus.position;
+        transform.localRotation = Quaternion.Euler(orbitAngles);
+    }
+
+    void OnValidate()
+    {
+        if (maxVerticalAngle < minVerticalAngle)
+        {
+            maxVerticalAngle = minVerticalAngle;
+        }
+    }
+
+    void LateUpdate()
+    {
+        UpdateFocusPoint();
+        ManualRotation();
+        Quaternion lookRotation;
+        if (ManualRotation() || AutomaticRotation())
+        {
+            ConstrainAngles();
+            lookRotation = Quaternion.Euler(orbitAngles);
+        }
+        else
+        {
+            lookRotation = transform.localRotation;
+        }
+        Vector3 lookDirection = lookRotation * Vector3.forward;
+        Vector3 lookPosition = focusPoint - lookDirection * distance;
+
+        Vector3 rectOffset = lookDirection * regularCamera.nearClipPlane;
+		Vector3 rectPosition = lookPosition + rectOffset;
+		Vector3 castFrom = focus.position;
+		Vector3 castLine = rectPosition - castFrom;
+		float castDistance = castLine.magnitude;
+		Vector3 castDirection = castLine / castDistance;
+
+        // check if camera collides with the environment and in case reduce the distance to the player
+        if (Physics.BoxCast(castFrom, CameraHalfExtends, castDirection, out RaycastHit hit, lookRotation, castDistance, obstructionMask))
+        {
+            rectPosition = castFrom + castDirection * hit.distance;
+			lookPosition = rectPosition - rectOffset;
+        }
+
+        transform.SetPositionAndRotation(lookPosition, lookRotation);
+    }
+
+    void UpdateFocusPoint()
+    {
+        previousFocusPoint = focusPoint;
+
+        Vector3 targetPoint = focus.position;
+
+        if (focusRadius > 0f)
+        {
+            float distance = Vector3.Distance(targetPoint, focusPoint);
+            if (distance > focusRadius)
+            {
+                focusPoint = Vector3.Lerp(targetPoint, focusPoint, focusRadius / distance);
+            }
+            if (distance > 0.01f && focusCentering > 0f)
+            {
+                focusPoint = Vector3.Lerp(targetPoint, focusPoint, Mathf.Pow(1f - focusCentering, Time.unscaledDeltaTime));
+            }
+        }
+        else
+        {
+            focusPoint = targetPoint;
+        }
+    }
+
+    bool ManualRotation()
+    {
+        float y = invertYAxis ? Input.GetAxis("Mouse Y") : -Input.GetAxis("Mouse Y");
+        float x = invertXAxis ? Input.GetAxis("Mouse X") : -Input.GetAxis("Mouse X");
+        Vector2 input = new Vector2(y, x);
+
+        const float e = 0.001f;
+        if (input.x < -e || input.x > e || input.y < -e || input.y > e)
+        {
+            orbitAngles += rotationSpeed * Time.unscaledDeltaTime * input;
+            lastManualRotationTime = Time.unscaledTime;
+            return true;
+        }
+
+        return false;
+    }
+
+    void ConstrainAngles()
+    {
+        orbitAngles.x =
+            Mathf.Clamp(orbitAngles.x, minVerticalAngle, maxVerticalAngle);
+
+        if (orbitAngles.y < 0f)
+        {
+            orbitAngles.y += 360f;
+        }
+        else if (orbitAngles.y >= 360f)
+        {
+            orbitAngles.y -= 360f;
+        }
+    }
+
+    bool AutomaticRotation()
+    {
+        if (Time.unscaledTime - lastManualRotationTime < alignDelay)
+        {
+            return false;
+        }
+
+        Vector2 movement = new Vector2(focusPoint.x - previousFocusPoint.x, focusPoint.z - previousFocusPoint.z);
+
+        float movementDeltaSqr = movement.sqrMagnitude;
+        if (movementDeltaSqr < 0.000001f)
+        {
+            return false;
+        }
+
+        float headingAngle = GetAngle(movement / Mathf.Sqrt(movementDeltaSqr));
+        float deltaAbs = Mathf.Abs(Mathf.DeltaAngle(orbitAngles.y, headingAngle));
+        float rotationChange = rotationSpeed * Mathf.Min(Time.unscaledDeltaTime, movementDeltaSqr);
+
+        if (deltaAbs < alignSmoothRange)
+        {
+            rotationChange *= deltaAbs / alignSmoothRange;
+        }
+        else if (180f - deltaAbs < alignSmoothRange)
+        {
+            rotationChange *= (180f - deltaAbs) / alignSmoothRange;
+        }
+
+        orbitAngles.y = Mathf.MoveTowardsAngle(orbitAngles.y, headingAngle, rotationChange);
+
+        return true;
+    }
+
+    static float GetAngle(Vector2 direction)
+    {
+        float angle = Mathf.Acos(direction.y) * Mathf.Rad2Deg;
+        return direction.x < 0f ? 360f - angle : angle; ;
+    }
+
+    Vector3 CameraHalfExtends
+    {
+        get
+        {
+            Vector3 halfExtends;
+            halfExtends.y =
+                regularCamera.nearClipPlane *
+                Mathf.Tan(0.5f * Mathf.Deg2Rad * regularCamera.fieldOfView);
+            halfExtends.x = halfExtends.y * regularCamera.aspect;
+            halfExtends.z = 0f;
+            return halfExtends;
+        }
+    }
+}
